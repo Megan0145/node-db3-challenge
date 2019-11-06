bd4ffa032d4b3f0cfcabf6737892fc20ccbd4124
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-06 14:31:05 +0000

    Helper method to get steps for given scheme (findSteps)

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require("../data/db-config.js");
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 };
 
 function find() {
@@ -13,4 +14,17 @@ function findById(id) {
   return db("schemes")
     .where({ id })
     .first();
+}
+
+function findSteps(schemeID) {
+  return db("steps as st")
+    .join("schemes as sch", "sch.id", "st.scheme_id")
+    .select(
+      "st.id as id",
+      "sch.scheme_name",
+      "st.step_number",
+      "st.instructions"
+    )
+    .where({ "st.scheme_id": schemeID })
+    .orderBy("st.step_number");
 }
